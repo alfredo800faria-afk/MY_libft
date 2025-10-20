d1b2117dbb641b085030310b1fcbb758f822ee94
alfredo800faria <alfredo800faria@gmail.com>
2025-10-20 02:07:42 +0100

    ft_toupper.c

---
+++ my_libft/ft_toupper.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+/* Linux man page: oupper() converts the letter c to upper case, if possible.*/
+
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (0);
+}
