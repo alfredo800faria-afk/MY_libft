d35053556f9dafa88bd578779a1b99ed709f2cc7
alfredo800faria <alfredo800faria@gmail.com>
2025-10-20 01:47:01 +0100

    ft_isalnum.c

---
+++ my_libft/ft_isalnum.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+/* checks for an alphanumeric character; it is equivalent to (isalpha(c) || isdigit(c)). */
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z')
+			|| (c >= 'A' && c <= 'Z')
+			|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
