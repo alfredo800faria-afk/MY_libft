f778fe31ee2b56befd2c180c0ee94fe67b45137e
alfredo800faria <alfredo800faria@gmail.com>
2025-10-20 01:55:21 +0100

    ft_isascii.c

---
+++ my_libft/ft_isascii.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+/* Manpage: checks whether c is a 7-bit unsigned char value that fits into 
+ * the ASCII character set. */
+
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
+}
