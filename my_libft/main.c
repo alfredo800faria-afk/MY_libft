d6ac2f47f0c965f69ddfd2d9927781de3fc4fbd7
alfredo800faria <alfredo800faria@gmail.com>
2025-10-20 01:45:37 +0100

    main.c

---
+++ my_libft/main.c
@@ -0,0 +1,94 @@
+#include "libft.h"
+#include <stdio.h>
+
+int	ft_ataoi(char *s)
+{
+	int	i;
+	int	res;
+	
+	i = 0;
+	res = 0;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		res = res * 10 + (s[i] - '0');
+		i++;
+	}
+	return (res * 1);
+}
+
+void	show_alpha(char *s)
+{
+		if (ft_isalpha(ft_ataoi(s)))
+			printf("True is Alpha\n");
+		else
+			printf("Is not Alpha\n");
+}
+
+void	show_isdigit(char *s)
+{
+		if (ft_isdigit(ft_ataoi(s)))
+			printf("True is isdigit\n");
+		else
+			printf("Is not isdigit\n");
+}
+
+void	show_isalnum(char *s)
+{
+		if (ft_isalnum(ft_ataoi(s)))
+			printf("True is isalnum\n");
+		else
+			printf("Is not isalnum\n");
+}
+
+void	show_isascii(char *s)
+{
+		if (ft_isascii(ft_ataoi(s)))
+			printf("True is Ascii\n");
+		else
+			printf("Is not Aascii\n");
+}
+
+void	show_isprint(char *s)
+{
+		if (ft_isprint(ft_ataoi(s)))
+			printf("True is Printable\n");
+		else
+			printf("Is not Printable\n");
+}
+
+void	show_tolower(char *s)
+{
+	char	c;
+
+	c = ft_tolower(ft_ataoi(s));
+	if (ft_tolower(ft_ataoi(s)))
+		printf("True is lower case %c\n", c);
+	else
+		printf("Is not lower case\n");
+}
+
+void	show_toupper(char *s)
+{
+	char	c;
+
+	c = ft_toupper(ft_ataoi(s));
+	if (ft_toupper(ft_ataoi(s)))
+		printf("True is lower case %c\n", c);
+	else
+		printf("Is not lower case\n");
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		show_alpha(argv[1]);
+		show_isdigit(argv[1]);
+		show_isalnum(argv[1]);
+		show_isascii(argv[1]);
+		show_isprint(argv[1]);
+		show_tolower(argv[1]);
+		show_toupper(argv[1]);
+	}
+	return (0);
+}
