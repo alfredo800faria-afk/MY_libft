b49293cf9fb276ffa9a7c7208fec35c9e18df703
alfredo800faria-afk <alfredo800faria@gmail.com>
2025-10-26 18:27:19 +0000

    Downgrade

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: srusso-b <srusso-b@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 15:12:12 by srusso-b          #+#    #+#             */
-/*   Updated: 2025/10/26 18:06:24 by srusso-b         ###   ########.fr       */
+/*   Updated: 2025/10/26 18:24:17 by srusso-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,15 @@ char	*ft_strchr(const char *str, int c)
 	return (NULL);
 }
 /*
-#include <stdio.h>
-
-int main()
+int main(void)
 {
-    char *str = "Hello, Marvin!";
+    const char *s = "Hello, Marvin!";
 
-    printf("%s\n", ft_strchr(str, 'o'));     // "o, Marvin!"
-    printf("%s\n", ft_strchr(str, 'z'));     // (null)
-    printf("%s\n", ft_strchr(str, '\0'));    // "" End string
-    printf("%s\n", ft_strchr(str, 'H'));     // "Hello, Marvin!"
-    return 0;
+    printf("Test 1: %s\n", ft_strchr(s, 'o'));     // "o, Marvin!"
+    printf("Test 2: %s\n", ft_strchr(s, 'z'));     // (null)
+    printf("Test 3: %s\n", ft_strchr(s, '\0'));    // (empty line)
+    printf("Test 4: %s\n", ft_strchr(s, 'H'));     // "Hello, Marvin!
+
+    return (0);
 }
 */
